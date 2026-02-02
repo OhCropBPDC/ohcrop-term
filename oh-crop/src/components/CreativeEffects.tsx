8eca246e3b0b772a9ad5a184a77f8fa4decc3552
BRoliix <162447225+BRoliix@users.noreply.github.com>
2026-02-02 17:39:49 +0400

    jj

--- oh-crop/src/components/CreativeEffects.tsx
+++ oh-crop/src/components/CreativeEffects.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { motion } from "framer-motion";
-import { memo, useMemo, useEffect, useState } from "react";
+import { memo, useEffect, useMemo, useState } from "react";
 
 interface FloatingShapeProps {
   delay?: number;
